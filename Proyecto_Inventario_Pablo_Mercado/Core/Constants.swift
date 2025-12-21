8c1821fd22ee387389de51e27786dd5c873acbfc
nandoxts <nandoxts01@gmail.com>
2025-12-20 20:10:08 -0500

    feat: Agregar constantes para la configuración de SendGrid en el archivo Constants.swift

--- Proyecto_Inventario_Pablo_Mercado/Core/Constants.swift
+++ Proyecto_Inventario_Pablo_Mercado/Core/Constants.swift
@@ -13,5 +13,10 @@ struct Constants {
     static let userId = "userId"
     static let role = "userRole"
     static let usersStore = "usersStore"
+    
+    // SendGrid API
+    static let sendGridApiKey = "SG.2oNZoKtcR5K2i7S01zuQ_S2_4Zw41cxhvhvx3Er1I003e1uGUAU3Yz2IFIags"
+    static let sendGridFromEmail = "inventario@tuempresa.com"
+    static let sendGridFromName = "Sistema de Inventario"
 }
 
